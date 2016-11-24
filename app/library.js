576426e583890df1dfa587050ca8f2ea10c95ac3
Vanessa Ejikeme <vanessa.ejikeme@gmail.com>
2016-11-24 10:54:15 +0100

    Implement function for Arithmetic and Geometric

--- app/library.js
+++ app/library.js
@@ -32,8 +32,24 @@ aritGeo:function(arr){
     return "Geometric";
   }
   return -1;
-  }
 
+},
+
+fizzBuzz:function(x)
+{
+  if (x % 3 === 0 && x % 5 === 0){
+    return 'FizzBuzz';
+  }
+  else if (x % 3 === 0){
+    return 'Fizz';
+  }
+  else if (x % 5 === 0){
+    return 'Buzz';
+  }
+  else {
+    return x;
+  }
 
  }
 
+}
